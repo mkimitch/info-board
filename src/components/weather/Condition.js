6846c3a78e314a32adeb46ee2dcd26cd2ad5390f
Mark Kimitch <mark.kimitch@gmail.com>
2020-08-12 23:50:34 -0500

    Minor update to use useEffect and useState

--- src/components/weather/Condition.js
+++ src/components/weather/Condition.js
@@ -1,14 +1,22 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useGlobalStore } from '../Store'
 
 export default function Condition() {
 	const { globalStore } = useGlobalStore()
+	const [weatherIcon, setWeatherIcon] = useState()
+	const [weatherIconAlt, setWeatherIconAlt] = useState()
 
+	useEffect(() => {
+		setWeatherIcon(globalStore.openWeatherJSON.current?.weather[0].icon)
+		setWeatherIconAlt(
+			globalStore.openWeatherJSON.current?.weather[0].description
+		)
+	}, [globalStore.openWeatherJSON])
 	return (
 		<>
 			<img
-				src={`http://openweathermap.org/img/wn/${globalStore.openWeatherJSON.current?.weather[0].icon}@2x.png`}
-				alt={globalStore.openWeatherJSON.current?.weather[0].description}
+				src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
+				alt={weatherIconAlt}
 			/>
 		</>
 	)
